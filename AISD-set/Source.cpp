cb1cd00e6827e3755d11942db7f4640160a3517e
DimNice <93138173+DimNice@users.noreply.github.com>
2022-04-11 11:21:56 +0300

    Update Source.cpp

--- AISD-set/Source.cpp
+++ AISD-set/Source.cpp
@@ -1,7 +1,8 @@
 #include "Set.h"
-#define menu "1)+\n2)-\n3)find\n4)print\n0)esc\n"
+#define menu "1)Добавить\n2)Удалить\n3)Проверка на наличие\n4)Печать\n0)Выход\n"
 int main()
 {
+	setlocale(LC_ALL, "Russian");
 	Set s;
 	unsigned menu_choice = 1;
 	double value;
@@ -13,16 +14,28 @@ int main()
 		{
 		case 1:
 			system("cls");
-			cout << "+: ";
+			cout << "Введите значение для добавления: ";
 			cin >> value;
-			(s.insert(int(value))) ? cout << "value " << int(value) << " added" << endl : cout << "value " << int(value) << "exist" << endl;
+			(s.insert(int(value))) ? cout << "Значение " << int(value) << " добавлено" << endl : cout << "Значение " << int(value) << "существует" << endl;
 			break;
 		case 2:
-
+			system("cls");
+			cout << "Введите значение для удаления: ";
+			cin >> value;
+			(s.erase(int(value))) ? cout << "Значение " << int(value) << " удалено" << endl : cout << "Значения " << int(value) << " нет" << endl;
+			break;
 		case 3:
-
+			system("cls");
+			cout << "Введите значение для проверки на наличие: ";
+			cin >> value;
+			(s.find(int(value))) ? cout << "Значение " << int(value) << " есть" << endl : cout << "Значения " << int(value) << " нет" << endl;
+			break;
 		case 4:
-
+			system("cls");
+			cout << "Множество:\n";
+			cout << s;
+			cout << endl;
+			break;
 		case 0:
 			system("cls");
 			break;
